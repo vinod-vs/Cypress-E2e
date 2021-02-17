dbe07132b872db6609c9e052ad7dee10dbbeab6d
Amit Rawat <arawat@woolworths.com.au>
2021-02-17 14:53:20 +1100

    cypress reporting using mochawesome and login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,11 +1,15 @@
-
 Cypress.Commands.add('loginb2c', (creds = {}) => {
-  cy.request({
-    url: Cypress.env('b2cLogin'),
-    method: 'POST',
-    body: {
-      email: creds.b2c.email,
-      password: creds.b2c.password
-    }
-  })
+    cy.request({
+        url: Cypress.env('b2cLogin'),
+        method: 'POST',
+        body: {
+            email: creds.b2c.email,
+            password: creds.b2c.password
+        }
+    })
+})
+Cypress.Commands.add('LoginViaApi', (shopper) => {
+    cy.request('POST', '/apis/ui/Login/loginwithcredential', shopper).then((response) => {
+        expect(response.body).to.have.property('LoginResult', 'Success')
+    })
 })
