e23744bdcd43d6c5f1f911cad2f83f08bc94afa8
Amit Rawat <arawat@woolworths.com.au>
2021-02-17 17:33:41 +1100

    modified ui login

--- cypress/integration/login_ui.spec.js
+++ cypress/integration/login_ui.spec.js
@@ -1,23 +1,18 @@
 /// <reference types="cypress" />
 
 describe('Perform Login via UI', () => {
+    before(() => {
+        cy.clearCookies({ domain: null })
+        cy.clearLocalStorage({ domain: null })
+    })
+
     beforeEach(() => {
         cy.fixture('b2cShoppers.json').as('b2cShopper')
     })
 
     it('Login', function() {
         for (var shopper of this.b2cShopper) {
-            cy.visit('shop/securelogin')
-
-            cy.get('#loginForm-Email').type(shopper.email)
-
-            cy.get('#loginForm-Password').type(shopper.password)
-
-            cy.get('.primary-legacy').click()
-
-            cy.contains('My Account').click()
-
-            cy.contains('Logout').click()
+            cy.LoginViaUi(shopper)
         }
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,8 +8,25 @@ Cypress.Commands.add('loginb2c', (creds = {}) => {
         }
     })
 })
+
 Cypress.Commands.add('LoginViaApi', (shopper) => {
-    cy.request('POST', '/apis/ui/Login/loginwithcredential', shopper).then((response) => {
+    cy.request('POST', Cypress.env('b2cLogin'), shopper).then((response) => {
         expect(response.body).to.have.property('LoginResult', 'Success')
     })
+})
+
+Cypress.Commands.add('LoginViaUi', (shopper) => {
+    cy.visit('shop/securelogin')
+
+    cy.get('#loginForm-Email').type(shopper.email)
+
+    cy.get('#loginForm-Password').type(shopper.password)
+
+    cy.get('.primary-legacy').click()
+
+    cy.get('#header-panel a.coreHeader-signupButton').contains('My Account').click()
+
+    cy.get('shared-navigation-menu a[queryparamshandling]').contains('Logout').click()
+
+    cy.url().should('eq', Cypress.config().baseUrl)
 })
