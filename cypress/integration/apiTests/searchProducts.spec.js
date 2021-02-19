875ffd3f7f00a0713eca17c57c660dfb0e7627db
Amit Rawat <arawat@woolworths.com.au>
2021-02-19 16:48:54 +1100

    standard fix

--- cypress/integration/apiTests/searchProducts.spec.js
+++ cypress/integration/apiTests/searchProducts.spec.js
@@ -6,26 +6,26 @@ import '../../support/api/login/login'
 import '../../support/api/search/search'
 
 describe('Perform Login via API', () => {
-    before(() => {
-        cy.clearCookies({ domain: null })
-        cy.clearLocalStorage({ domain: null })
+  before(() => {
+    cy.clearCookies({ domain: null })
+    cy.clearLocalStorage({ domain: null })
+  })
+
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce('w-rctx', 'akaalb_uatsite', 'akavpau_uatsite', 'INGRESSCOOKIE', 'uat-wow-auth-token', 'wow-auth-token')
+
+    cy.fixture('b2cShoppers.json').as('b2cShopper')
+  })
+
+  it('Search product', () => {
+    cy.loginViaApi(shoppers[0])
+
+    searchBody.SearchTerm = 'Milk'
+
+    cy.apiSearch(searchBody).then((response) => {
+      expect(response.SearchResultsCount).to.be.greaterThan(0)
+
+      cy.log(response.SearchResultsCount)
     })
-
-    beforeEach(() => {
-        Cypress.Cookies.preserveOnce('w-rctx', 'akaalb_uatsite', 'akavpau_uatsite', 'INGRESSCOOKIE', 'uat-wow-auth-token', 'wow-auth-token')
-
-        cy.fixture('b2cShoppers.json').as('b2cShopper')
-    })
-
-    it('Search product', () => {
-        cy.loginViaApi(shoppers[0])
-
-        searchBody.SearchTerm = 'Milk'
-
-        cy.apiSearch(searchBody).then((response) => {
-            expect(response.SearchResultsCount).to.be.greaterThan(0)
-
-            cy.log(response.SearchResultsCount)
-        })
-    })
+  })
 })
