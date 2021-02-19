2e09aeb1a6715eace9996f2ea44927c29ce78473
Amit Rawat <arawat@woolworths.com.au>
2021-02-19 16:06:06 +1100

    name change for login ui test

---
+++ cypress/integration/uiTests/login.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+import shoppers from '../../fixtures/b2cShoppers.json'
+import '../../support/ui/login/login'
+
+describe('Perform Login via UI', () => {
+    before(() => {
+        cy.clearCookies({ domain: null })
+        cy.clearLocalStorage({ domain: null })
+    })
+
+    beforeEach(() => {
+        cy.fixture('b2cShoppers.json').as('b2cShopper')
+    })
+
+    shoppers.forEach((shopper) => {
+        it('Login as ' + shopper.email, () => {
+            cy.loginViaUi(shopper)
+        })
+    })
+})
