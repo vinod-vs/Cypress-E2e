5273edc23cff73274345f3eff0113838c0477b2f
Amit Rawat <arawat@woolworths.com.au>
2021-02-19 16:46:31 +1100

    moved search request body to fixture file

--- cypress/integration/apiTests/searchProducts.spec.js
+++ cypress/integration/apiTests/searchProducts.spec.js
@@ -1,28 +1,31 @@
 /// <reference types="cypress" />
 
 import shoppers from '../../fixtures/b2cShoppers.json'
+import searchBody from '../../fixtures/productSearch.json'
 import '../../support/api/login/login'
 import '../../support/api/search/search'
 
 describe('Perform Login via API', () => {
-  before(() => {
-    cy.clearCookies({ domain: null })
-    cy.clearLocalStorage({ domain: null })
-  })
-
-  beforeEach(() => {
-    Cypress.Cookies.preserveOnce('w-rctx', 'akaalb_uatsite', 'akavpau_uatsite', 'INGRESSCOOKIE', 'uat-wow-auth-token', 'wow-auth-token')
-
-    cy.fixture('b2cShoppers.json').as('b2cShopper')
-  })
-
-  it('Search product', () => {
-    cy.loginViaApi(shoppers[0])
-
-    cy.apiSearch('Milk').then((response) => {
-      expect(response.SearchResultsCount).to.be.greaterThan(0)
-
-      cy.log(response.SearchResultsCount)
+    before(() => {
+        cy.clearCookies({ domain: null })
+        cy.clearLocalStorage({ domain: null })
     })
-  })
+
+    beforeEach(() => {
+        Cypress.Cookies.preserveOnce('w-rctx', 'akaalb_uatsite', 'akavpau_uatsite', 'INGRESSCOOKIE', 'uat-wow-auth-token', 'wow-auth-token')
+
+        cy.fixture('b2cShoppers.json').as('b2cShopper')
+    })
+
+    it('Search product', () => {
+        cy.loginViaApi(shoppers[0])
+
+        searchBody.SearchTerm = 'Milk'
+
+        cy.apiSearch(searchBody).then((response) => {
+            expect(response.SearchResultsCount).to.be.greaterThan(0)
+
+            cy.log(response.SearchResultsCount)
+        })
+    })
 })
--- cypress/support/api/search/search.js
+++ cypress/support/api/search/search.js
@@ -1,13 +1,4 @@
-Cypress.Commands.add('apiSearch', (searchTerm) => {
-  const searchBody = {
-    Filters: [],
-    IsSpecial: false,
-    PageNumber: 1,
-    PageSize: 36,
-    SearchTerm: searchTerm,
-    SortType: 'TraderRelevance'
-  }
-
+Cypress.Commands.add('apiSearch', (searchBody) => {
   cy.request('POST', Cypress.env('productSearchEndpoint'), searchBody).then((response) => {
     return response.body
   })
