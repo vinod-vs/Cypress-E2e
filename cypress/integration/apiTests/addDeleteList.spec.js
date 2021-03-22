8c8785b4e5c1adca30295a07eca7a4764e08f6db
Amit Rawat <arawat@woolworths.com.au>
2021-03-23 09:25:01 +1100

    minor change in pretest

--- cypress/integration/apiTests/addDeleteList.spec.js
+++ cypress/integration/apiTests/addDeleteList.spec.js
@@ -7,28 +7,28 @@ import '../../support/api/list/addList'
 import '../../support/api/list/deleteList'
 
 describe('Add a new list, add items in the list and delete the list', () => {
-  before(() => {
-    cy.clearCookies({ domain: null })
-    cy.clearLocalStorage({ domain: null })
-  })
-
-  it('Should create a new list, add items in the list and delete the list', () => {
-    cy.loginViaApi(shopper.b2c).then((response) => {
-      expect(response).to.have.property('LoginResult', 'Success')
+    before(() => {
+        cy.clearCookies({ domain: null })
+        cy.clearLocalStorage({ domain: null })
     })
 
-    cy.addList(listName).then((response) => {
-      expect(response).to.have.property('Message', 'The new list has been created sucessfully')
+    it('Should create a new list, add items in the list and delete the list', () => {
+        cy.loginViaApi(shopper.b2c).then((response) => {
+            expect(response).to.have.property('LoginResult', 'Success')
+        })
 
-      cy.wrap(response.ListId).as('listId')
+        cy.addList(listName).then((response) => {
+            expect(response).to.have.property('Message', 'The new list has been created sucessfully')
+
+            cy.wrap(response.ListId).as('listId')
+        })
+
+        // TODO: Adding items in the list is in progress
+
+        cy.get('@listId').then(listId => {
+            cy.deleteList(listId).then((response) => {
+                expect(response.status).to.eq(200)
+            })
+        })
     })
-
-    // TODO: Adding items in the list is in progress
-
-    cy.get('@listId').then(listId => {
-      cy.deleteList(listId).then((response) => {
-        expect(response.status).to.eq(200)
-      })
-    })
-  })
 })
