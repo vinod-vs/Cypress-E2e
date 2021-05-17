69efdc7180b5b3787e627463f254612a9a630b20
ArpithaArun <vsiragoan@woolworths.com.au>
2021-05-17 18:09:54 +1000

    Adding test to signup as a new user

---
+++ cypress/support/signUp/api/commands/signUp.js
@@ -0,0 +1,13 @@
+Cypress.Commands.add('signUpViaApi' , (userInfo) => {
+  cy.api({ url: '/' }).then((response) => {
+    expect(response.status).to.eq(200)
+  })
+
+  cy.api({
+    method: 'POST',
+    url: Cypress.env('signUpEndpoint'),
+    body: userInfo
+  }).then((response) => {
+    return response
+  })
+})
