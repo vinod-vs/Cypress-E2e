43a06bc908643d4c83c5a65adca51157121a3895
Emma Wu <ewu1@woolworths.com.au>
2021-08-31 16:39:22 +1000

    update test for CoolService

--- cypress/support/coolService/api/coolServiceApi.js
+++ cypress/support/coolService/api/coolServiceApi.js
@@ -1,6 +1,9 @@
-Cypress.Commands.add('getCountryOfOrigin', (CountryOfOrginBody) => {
-    cy.request('GET', Cypress.env('coolServiceEndpoint'), CountryOfOrginBody).then((response) => {
+Cypress.Commands.add('getCountryOfOrigin', () => {
+
+  let stockcode=405010; // Need be updated with more stockcodes
+  let code="HpEM1cUoXOzjTBWdT2lM13SoeMfRQa2IrwOMnNddbQNH6aaITeCI6A=="; 
+
+    cy.request('GET', Cypress.env('coolServiceEndpoint') + "&" + stockcode + "&" + code).then((response) => {
       return response.body
     })
   })
-  
