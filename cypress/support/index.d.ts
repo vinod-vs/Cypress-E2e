eb596118cbdf65d3587feaffe66530aaf1653997
xrcnf <rclayton@woolworths.com.au>
2021-10-14 16:54:17 +1100

    Add xpath type to tsconfig, & cmds to index.d.ts. Change test file from js -> ts

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,6 +1,18 @@
 declare namespace Cypress {
   interface Chainable {
     // Cypress custom commands
-    clearCookies(value: any): void;  
+    clearCookies(value: any): void;
+    clearLocalStorage(value: any): void;
+    loginViaApi(value: any): void;
+    searchPickupDTBStores(arg0: any, arg1: any): any;
+    getFulfilmentWindowViaApi(arg0: any): any;
+    completeWindowFulfilmentViaApi(): any;
+    clearTrolley(): void;
+    addAvailableNonRestrictedPriceLimitedWowItemsToTrolley(arg0: any, arg1: any): void;
+    navigateToCheckout(): any;
+    navigatingToCreditCardIframe(): any;
+    creditcardPayment(arg0: any, arg1: any): any;
+    digitalPay(arg0: any): any;
+    confirmOrder(arg0: any): any;  
   }
 }
