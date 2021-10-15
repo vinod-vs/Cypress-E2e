5752aea71ec1028bb63f665acde17f020f53262b
amarnath-wow <amylaporedwarakanath@woolworths.com.au>
2021-10-15 12:14:34 +1100

    SM test case

---
+++ cypress/integration/stg_testing/sm.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+import TestFilter from '../../support/TestFilter'
+
+// TestFilter(["stg_ms"], () => {
+  describe("Verifying SM at Stage", () => {
+    it("Load Stage", () => {
+      cy.visit("https://adminstgsite.woolworths.com.au/Manage");
+      // Do Google Oauth
+    });
+
+    // create test to submit a classic promotion
+  });
+// });
