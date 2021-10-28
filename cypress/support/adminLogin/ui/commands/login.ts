37bb1a020551396ec258aada24087d611c259663
Jonathan Simeon Villao <jvillao@woolworths.com.au>
2021-10-28 21:00:40 +1100

    removed renamed files

--- cypress/support/adminLogin/ui/commands/login.ts
+++
@@ -1,7 +0,0 @@
-import {loginPage} from '../pageObjects/LoginPage';
-
-Cypress.Commands.add('adminLoginViaUi', (loginDetails) => {
-    loginPage.getEmailAddress().type(loginDetails.email)
-    loginPage.getPassword().type(loginDetails.password)
-    loginPage.getLoginButton().click()
-})
