7fc4083fa6f831d3420eeff646b1aed5a9dca267
Sucharitha Sunkara <ssunkara1@woolworths.com.au>
2021-11-15 17:32:45 +1100

    update tests

--- cypress/integration/promotionEngines/apiTests/verifyClassiProductPromotionEvaluation.spec.ts
+++ cypress/integration/promotionEngines/apiTests/verifyClassiProductPromotionEvaluation.spec.ts
@@ -1,5 +1,5 @@
 import shoppers from '../../../fixtures/promotionEngines/shoppers.json'
-import search from '../../../fixtures/promotionEngines/search.json'
+import promotions from '../../../fixtures/promotionEngines/promotions.json'
 import '../../../support/login/api/commands/login'
 import '../../../support/sideCart/api/commands/clearTrolley'
 import '../../../support/sideCart/api/commands/addItemsToTrolley'
@@ -8,7 +8,7 @@ import TestFilter from '../../../support/TestFilter'
 const searchTerm = '192674'
 const platform = Cypress.env('b2cPlatform')
 
-TestFilter(['B2C-API'], () => {
+TestFilter(['PES-API'], () => {
   describe('[API] Verify Classic Product Promotions', () => {
     before(() => {
       cy.clearCookies({ domain: null })
@@ -16,18 +16,41 @@ TestFilter(['B2C-API'], () => {
     })
 
 
-    it('Verify the ClassicProduct promotion price is applied for the item', () => {
+    it('Verify the Classic Product promotion price is applied for the item - $OFF', () => {
       // Login using shopper saved in the fixture and verify it's successful
       cy.loginViaApi(shoppers.PESAccount1).then((response: any) => {
         expect(response).to.have.property('LoginResult', 'Success')
       })
   
       // Set the Delivery address and add the items to Trolley
-      cy.addAvailableNonRestrictedWowItemsToTrolley(search.stockcode).then((response:any)=> {
-        expect(response.AvailableItems[0].SalePrice.toString()).to.be.eqls(search.SalePrice)
+      cy.addAvailableNonRestrictedWowItemsToTrolley(promotions.ClassicProductPromotions[0].stockcode).then((response:any)=> {
+        expect(response.AvailableItems[0].SalePrice.toString()).to.be.eqls(promotions.ClassicProductPromotions[0].SalePrice)
         })
-        
       })
+
+      it('Verify the ClassicProduct promotion price is applied for the item - %OFF', () => {
+        // Login using shopper saved in the fixture and verify it's successful
+        cy.loginViaApi(shoppers.PESAccount1).then((response: any) => {
+          expect(response).to.have.property('LoginResult', 'Success')
+        })
+    
+        // Set the Delivery address and add the items to Trolley
+        cy.addAvailableNonRestrictedWowItemsToTrolley(promotions.ClassicProductPromotions[1].stockcode).then((response:any)=> {
+          expect(response.AvailableItems[0].SalePrice.toString()).to.be.eqls(promotions.ClassicProductPromotions[1].SalePrice)
+          })
+        })
+
+        it('Verify the ClassicProduct promotion price is applied for the item - Fixed Amount and Packaged Price', () => {
+          // Login using shopper saved in the fixture and verify it's successful
+          cy.loginViaApi(shoppers.PESAccount1).then((response: any) => {
+            expect(response).to.have.property('LoginResult', 'Success')
+          })
+      
+          // Set the Delivery address and add the items to Trolley
+          cy.addAvailableNonRestrictedWowItemsToTrolley(promotions.ClassicProductPromotions[1].stockcode).then((response:any)=> {
+            expect(response.AvailableItems[0].SalePrice.toString()).to.be.eqls(promotions.ClassicProductPromotions[1].SalePrice)
+            })
+          })
     })
   
 })
