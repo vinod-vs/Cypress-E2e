a884b27573ffc771d2b072d26c68bd4790b90393
Sucharitha Sunkara <ssunkara1@woolworths.com.au>
2021-11-15 16:25:27 +1100

    Update test file

--- cypress/integration/promotionEngines/apiTests/verifyClassiProductPromotionEvaluation.spec.ts
+++ cypress/integration/promotionEngines/apiTests/verifyClassiProductPromotionEvaluation.spec.ts
@@ -1,12 +1,10 @@
-/// <reference types="cypress" />
-/* eslint-disable no-unused-expressions */
-
 import shoppers from '../../../fixtures/promotionEngines/shoppers.json'
+import '../../../support/login/api/commands/login'
 import '../../../support/sideCart/api/commands/clearTrolley'
 import '../../../support/sideCart/api/commands/addItemsToTrolley'
 import TestFilter from '../../../support/TestFilter'
 
-const searchTerm = '211377'
+const searchTerm = '192674'
 const platform = Cypress.env('b2cPlatform')
 
 TestFilter(['B2C-API'], () => {
@@ -19,13 +17,13 @@ TestFilter(['B2C-API'], () => {
 
     it('Verify the ClassicProduct promotion price is applied for the item', () => {
       // Login using shopper saved in the fixture and verify it's successful
-      cy.loginViaApi(shoppers.PESAccount1).then((response: any) => {
+      cy.loginViaApi(shoppers.PESAccount2).then((response: any) => {
         expect(response).to.have.property('LoginResult', 'Success')
       })
   
       // Set the Delivery address and add the items to Trolley
       cy.addAvailableNonRestrictedWowItemsToTrolley(searchTerm).then((response:any)=> {
-          expect(response).to.have.property('Success', true)
+        expect(response.AvailableItems[0].SalePrice).to.be.eqls(4.2)
         })
         
       })
