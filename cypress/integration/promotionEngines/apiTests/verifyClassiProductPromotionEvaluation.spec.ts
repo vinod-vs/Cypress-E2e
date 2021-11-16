17a266de39b16e29f74e19efd9372c9cdd1f2d4e
Sucharitha Sunkara <ssunkara1@woolworths.com.au>
2021-11-16 18:00:39 +1100

    Latest changes

--- cypress/integration/promotionEngines/apiTests/verifyClassiProductPromotionEvaluation.spec.ts
+++ cypress/integration/promotionEngines/apiTests/verifyClassiProductPromotionEvaluation.spec.ts
@@ -13,46 +13,46 @@ TestFilter(['PES','API'], () => {
     before(() => {
       cy.clearCookies({ domain: null })
       cy.clearLocalStorage({ domain: null })
+
+      // Login using shopper saved in the fixture and verify it's successful
+      cy.loginViaApi(shoppers.PESAccount1).then((response: any) => {
+        expect(response).to.have.property('LoginResult', 'Success')
+      })
       cy.clearTrolley().then((response:any) => {
         expect(response).to.have.property('TrolleyItemCount',0)
       })
     })
 
     it('Verify the Classic Product promotion price is applied for the item - $OFF', () => {
-      // Login using shopper saved in the fixture and verify it's successful
-      cy.loginViaApi(shoppers.PESAccount1).then((response: any) => {
-        expect(response).to.have.property('LoginResult', 'Success')
-      })
-  
       // Set the Delivery address and add the items to Trolley
-      cy.addAvailableNonRestrictedWowItemsToTrolley(promotions.ClassicProductPromotions[0].stockcode).then((response:any)=> {
-        expect(response.AvailableItems[0].SalePrice.toString()).to.be.eqls(promotions.ClassicProductPromotions[0].SalePrice)
+      cy.addAvailableItemsToTrolley(promotions.ClassicProductPromotions[0].stockcode.toString(),promotions.ClassicProductPromotions[0].Quantity).then((response:any)=> {
+        expect(response.AvailableItems[0].SalePrice).to.be.eqls(promotions.ClassicProductPromotions[0].SalePrice)
         })
       })
 
       it('Verify the ClassicProduct promotion price is applied for the item - %OFF', () => {
-        // Login using shopper saved in the fixture and verify it's successful
-        cy.loginViaApi(shoppers.PESAccount1).then((response: any) => {
-          expect(response).to.have.property('LoginResult', 'Success')
-        })
-    
+   
         // Set the Delivery address and add the items to Trolley
-        cy.addAvailableNonRestrictedWowItemsToTrolley(promotions.ClassicProductPromotions[1].stockcode).then((response:any)=> {
-          expect(response.AvailableItems[0].SalePrice.toString()).to.be.eqls(promotions.ClassicProductPromotions[1].SalePrice)
+        cy.addAvailableItemsToTrolley(promotions.ClassicProductPromotions[1].stockcode.toString(),promotions.ClassicProductPromotions[1].Quantity).then((response:any)=> {
+          expect(response.AvailableItems[0].SalePrice).to.be.eqls(promotions.ClassicProductPromotions[1].SalePrice)
           })
         })
 
-        it('Verify the ClassicProduct promotion price is applied for the item - Fixed Amount and Packaged Price', () => {
-          // Login using shopper saved in the fixture and verify it's successful
-          cy.loginViaApi(shoppers.PESAccount1).then((response: any) => {
-            expect(response).to.have.property('LoginResult', 'Success')
-          })
+        it('Verify the ClassicProduct promotion price is applied for the item - Fixed Amount', () => {
       
           // Set the Delivery address and add the items to Trolley
-          cy.addAvailableNonRestrictedWowItemsToTrolley(promotions.ClassicProductPromotions[1].stockcode).then((response:any)=> {
-            expect(response.AvailableItems[0].SalePrice.toString()).to.be.eqls(promotions.ClassicProductPromotions[1].SalePrice)
+          cy.addAvailableItemsToTrolley(promotions.ClassicProductPromotions[2].stockcode.toString(),promotions.ClassicProductPromotions[2].Quantity).then((response:any)=> {
+            expect(response.AvailableItems[0].SalePrice).to.be.eqls(promotions.ClassicProductPromotions[2].SalePrice)
             })
           })
+
+          it('Verify the ClassicProduct promotion price is applied for the item - Package Price', () => {
+      
+            // Set the Delivery address and add the items to Trolley
+            cy.addAvailableItemsToTrolley(promotions.ClassicProductPromotions[3].stockcode.toString(),promotions.ClassicProductPromotions[3].Quantity).then((response:any)=> {
+              expect(response.AvailableItems[0].SalePrice).to.be.eqls(promotions.ClassicProductPromotions[3].SalePrice)
+              })
+            })
     })
   
 })
