10acde51975385ee0c7871df879f13cb2a37da83
Rajiv Jagadish <z1145621@wowcorp.com.au>
2021-12-06 06:40:51 +0000

    Merged PR 44941: Refactoring sm refund tests with retry logic
    Refactoring sm refund tests with retry logic

--- cypress/support/orderPaymentService/api/commands/refunds.ts
+++ cypress/support/orderPaymentService/api/commands/refunds.ts
@@ -1,33 +1,92 @@
-Cypress.Commands.add('getAllRefundsByOrderId', (traderOrderId) => {
-  let endpoint = String(Cypress.env('orderPaymentServiceRefundsEndPoint'))
-  endpoint = endpoint.replace('TRADER_ORDER_ID', traderOrderId)
+/* eslint-disable no-unused-expressions */
+/// <reference types="cypress" />
+import "../../../utilities/api/apiUtilities";
+
+Cypress.Commands.add("getAllRefundsByOrderId", (traderOrderId) => {
+  let endpoint = String(Cypress.env("orderPaymentServiceRefundsEndPoint"));
+  endpoint = endpoint.replace("TRADER_ORDER_ID", traderOrderId);
   cy.api({
-    method: 'GET',
-    url: endpoint
+    method: "GET",
+    url: endpoint,
   }).then((response: any) => {
-    expect(response.status).to.eq(200)
-    return response.body
-  })
-})
+    expect(response.status).to.eq(200);
+    return response.body;
+  });
+});
 
-Cypress.Commands.add('getAllRefundPaymentsByRefundId', (refundId) => {
-  let endpoint = String(Cypress.env('orderPaymentServiceRefundPaymentsEndpoint'))
-  endpoint = endpoint.replace('REFUND_ID', refundId)
+Cypress.Commands.add("getAllRefundPaymentsByRefundId", (refundId) => {
+  let endpoint = String(
+    Cypress.env("orderPaymentServiceRefundPaymentsEndpoint")
+  );
+  endpoint = endpoint.replace("REFUND_ID", refundId);
   cy.api({
-    method: 'GET',
-    url: endpoint
+    method: "GET",
+    url: endpoint,
   }).then((response: any) => {
-    expect(response.status).to.eq(200)
-    return response.body
-  })
-})
+    expect(response.status).to.eq(200);
+    return response.body;
+  });
+});
 
-Cypress.Commands.add('findCCRefundPayment', (refundPaymentsDetails, refundAmount) => {
-  // Credit card will be refunded as credit card, otherwise store credit
-  return cy.wrap(refundPaymentsDetails.results.some((result: { type: string; total: number }) => (result.type === 'CreditCard' || result.type === 'StoreCreditCCFail') && result.total === refundAmount))
-})
+Cypress.Commands.add(
+  "findCCRefundPayment",
+  (refundPaymentsDetails, refundAmount) => {
+    // Credit card will be refunded as credit card, otherwise store credit
+    return cy.wrap(
+      refundPaymentsDetails.results.some(
+        (result: { type: string; total: number }) =>
+          (result.type === "CreditCard" ||
+            result.type === "StoreCreditCCFail") &&
+          result.total === refundAmount
+      )
+    );
+  }
+);
 
-Cypress.Commands.add('findSCRefundPayment', (refundPaymentsDetails, refundAmount) => {
-  return cy.wrap(refundPaymentsDetails.results.some((result: { type: string; total: number }) => result.type === 'StoreCredit' && result.total === refundAmount))
-})
+Cypress.Commands.add(
+  "findSCRefundPayment",
+  (refundPaymentsDetails, refundAmount) => {
+    return cy.wrap(
+      refundPaymentsDetails.results.some(
+        (result: { type: string; total: number }) =>
+          result.type === "StoreCredit" && result.total === refundAmount
+      )
+    );
+  }
+);
 
+Cypress.Commands.add(
+  "getAllRefundPaymentsByRefundIdWithRetry",
+  (refundId, retryOptions) => {
+    let endpoint = String(
+      Cypress.env("orderPaymentServiceRefundPaymentsEndpoint")
+    );
+    endpoint = endpoint.replace("REFUND_ID", refundId);
+    cy.retryRequest(endpoint, {
+      method: "GET",
+      retries: retryOptions.retries,
+      timeout: retryOptions.timeout,
+      function: retryOptions.function,
+    }).then((response: any) => {
+      expect(response.status).to.eq(200);
+      return response.body;
+    });
+  }
+);
+
+Cypress.Commands.add(
+  "getAllRefundsByOrderIdWithRetry",
+  (traderOrderId, retryOptions) => {
+    let endpoint = String(Cypress.env("orderPaymentServiceRefundsEndPoint"));
+    endpoint = endpoint.replace("TRADER_ORDER_ID", traderOrderId);
+    cy.retryRequest(endpoint, {
+      method: "GET",
+      retries: retryOptions.retries,
+      timeout: retryOptions.timeout,
+      function: retryOptions.function,
+    }).then((response: any) => {
+      expect(response.status).to.eq(200);
+      return response.body;
+    });
+  }
+);
