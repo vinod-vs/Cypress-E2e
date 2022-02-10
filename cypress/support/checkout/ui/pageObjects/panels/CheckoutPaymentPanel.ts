5ca99cdf31922f6a8ffef291bfc4741d5c256b31
Jonathan Simeon Villao <jvillao@woolworths.com.au>
2022-02-11 10:24:34 +1100

    Add order promotion with percentage discount test

--- cypress/support/checkout/ui/pageObjects/panels/CheckoutPaymentPanel.ts
+++ cypress/support/checkout/ui/pageObjects/panels/CheckoutPaymentPanel.ts
@@ -97,6 +97,10 @@ export class CheckoutPaymentPanel{
         return this.getAmountElementByPaymentTitle('Service fee discount')
     }
 
+    getPaymentDeliveryFeeAmountElement () {
+        return this.getAmountElementByPaymentTitle('Delivery fee')
+    }
+
     getPaymentDeliveryFeeDiscountAmountElement () {
         return this.getAmountElementByPaymentTitle('Delivery fee discount')
     }
