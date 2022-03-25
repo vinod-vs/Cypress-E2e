8c66a71d4f3828633e83ae7308c532a12e1cef36
Anoop Singh <z1165027@wowcorp.com.au>
2022-03-25 04:41:13 +0000

    Merged PR 49860: Fixing Failure of RP-5469
    This PR solves the failure of test scenario '_E2E-Regression-EMCancellationWithWowDispatchAndSMReIssue.ts_'
    you will see few commented code, please ignore that, because that's a part of _ongoing development work_.
    
    1- _Please ignore_ 'package-lock.json' , as I've updated the Cypress version to 8.7.0 so that 'select' commands could work fine as it is supported in the new version, so, package-lock.json was updated too.

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -109,7 +109,7 @@ declare namespace Cypress {
     signUpUser(signupdetails: any): any;
     subscribeToDUMonthlyPersonal(userinfor: any): any;
     get2FACode(shopper: any): Cypress.Chainable<string>;
-    performReIssueOnWowOrderOnSM(isMarketOnly: boolean): any;
+    performReIssueOnWowOrderOnSM(): any;
     wowDispatchUpdateCompletedOrder(shopperId:any , orderId:any, WoolworthsSubtotal:any, testData:any): any;
     checkForOrderPlacementErrorsAndThrow(paymentResponse: any): any;
     availableDigitalPaymentInstruments(): any;
