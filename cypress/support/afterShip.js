bbfcb1d2528aa75f2f62ba6ff9b81eaa55d843d0
Sigamani Merlin <manand3@woolworths.com.au>
2022-04-14 10:01:05 +1000

    updated dynamic values in Afterconsignmnetreq

--- cypress/support/afterShip.js
+++ cypress/support/afterShip.js
@@ -7,8 +7,14 @@ import getConsignmentWebhook from '../fixtures/afterShip.json'
 Cypress.Commands.add('invokeconsignmentwebhook', (postageTrackingnumber, status) => {
   const requestBody = getConsignmentWebhook
   const secret = String(Cypress.env('consignmentaftershipAPIKey'))
+  const idVal = Math.floor(Math.random() * (9 * (Math.pow(10, 5)))) + (Math.pow(10, 5))
+  const eventIdVal = Math.floor(Math.random() * (9 * (Math.pow(10, 5)))) + (Math.pow(10, 5))
+  const tsVal = Math.floor(Math.random() * (9 * (Math.pow(10, 5)))) + (Math.pow(10, 5))
+  requestBody.event_id = "17e03e7d-a969-455b-99eb-"+eventIdVal
+  requestBody.msg.id = idVal
   requestBody.msg.tracking_number = postageTrackingnumber
   requestBody.msg.tag = status
+  requestBody.ts = tsVal
   const signatureRawData = JSON.stringify(requestBody)
   const CryptoJS = require('crypto-js')
   const signature = CryptoJS.enc.Utf8.parse(signatureRawData)
