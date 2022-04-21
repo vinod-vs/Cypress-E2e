7be67f69bcf093c48d975fb4f75f252ec5d235df
rjagadish <rjagadish@woolworths.com.au>
2022-04-21 11:33:39 +1000

    Adding Email Notification Verifiation Test

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -167,5 +167,6 @@ declare namespace Cypress {
     generateRandomString(numChars?: number): Cypress.Chainable<string>;
     changeDateFormatToAddSlash(date: any): any;
     searchCustomerByEmailInSM(email: any): void;
+    getEmailDetails(emailId: string, expectedEmailSubject: string): any;
   }
 }
