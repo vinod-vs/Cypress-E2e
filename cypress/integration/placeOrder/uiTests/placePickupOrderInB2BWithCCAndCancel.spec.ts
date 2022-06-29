6574be35255496aebc7228b25a1dc5edd7253305
Shrote Pranali <pshrote1@woolworths.com.au>
2022-06-29 11:34:30 +1000

    Updated filename and added new test for B2B

--- cypress/integration/placeOrder/uiTests/placePickupOrderInB2BWithCCAndCancel.spec.ts
+++ cypress/integration/placeOrder/uiTests/placePickupOrderInB2BWithCCAndCancel.spec.ts
@@ -14,31 +14,24 @@ import '../../../support/checkout/api/commands/navigateToCheckout'
 import '../../../support/checkout/api/commands/confirmOrder'
 import '../../../support/payment/api/commands/creditcard'
 import '../../../support/payment/api/commands/digitalPayment'
-import '../../../support/delivery/api/commands/options'
-import '../../../support/address/api/commands/searchSetValidateAddress'
-import '../../../support/login/ui/commands/login'
 import '../../../support/checkout/api/commands/checkoutHelper'
+import { fulfilmentType } from '../../../fixtures/checkout/fulfilmentType.js'
+import { windowType } from '../../../fixtures/checkout/fulfilmentWindowType.js'
 import { MyOrderPage } from '../../../support/myOrder/ui/pageObjects/MyOrderPage'
-import { fulfilmentType } from '../../../fixtures/checkout/fulfilmentType'
-import { windowType } from '../../../fixtures/checkout/fulfilmentWindowType'
 import { onOrderDetailsPage } from '../../../support/myOrder/ui/pageObjects/OrderDetailsPage'
-import { onSideCartPage } from '../../../support/sideCart/ui/pageObjects/SideCartPage'
-import { onCheckoutPage } from '../../../support/checkout/ui/pageObjects/CheckoutPage'
-import { onOrderConfirmationPage } from '../../../support/orderConfirmation/ui/pageObjects/OrderConfirmationPage'
-import { onSearchResultsPage } from '../../../support/search/ui/pageObjects/SearchResultsPage'
 
-const searchTerm = 'pantry'
-const trolleyThreshold = 30.0
+const searchTerm = 'baby'
+const trolleyThreshold = 50.0
 const platform = Cypress.env('b2bPlatform')
 
 TestFilter(['B2B', 'UI'], () => {
-  describe('[UI] Place a pickup order in B2B with credit card Payment and amend the order', () => {
+  describe('[UI] Place a pickup order in B2B with credit card payment and cancel the order', () => {
     // pre-requisite to clear all cookies before login
     before(() => {
       cy.clearCookies({ domain: null })
       cy.clearLocalStorage({ domain: null })
     })
-    it('Should place a pickup order on Woolworths at Work website using Credit Card as payment option and amend the order', () => {
+    it('Should place a pickup order on Woolworths at Work website using Credit Card as payment option and Cancel the order', () => {
       cy.loginViaApi(shopper).then((response: any) => {
         expect(response).to.have.property('LoginResult', 'Success')
       })
@@ -89,77 +82,29 @@ TestFilter(['B2B', 'UI'], () => {
         (confirmOrderResponse: any) => {
           // Save the Order Id of the order placed
           const orderId = confirmOrderResponse.Order.OrderId
-          cy.wait(3000)
+          cy.wait(500)
 
           // Navigate to UI - My order page
           cy.visit('/shop/myaccount/myorders')
+          cy.wait(5000)
           cy.reload()
-          cy.wait(500)
 
           // Passing the orderId to the Page object constructor
           const onMyOrderPage = new MyOrderPage(orderId)
 
-          // Get to the Order details on My Orders page and Change the order
+          // Get to the Order details on My Orders page and cancel the order
           onMyOrderPage.getMyOrderNumber().should('contain', orderId)
           onMyOrderPage.getViewOrderDetailsLink().click()
-          onOrderDetailsPage.getChangeOrderButton().click()
-          onOrderDetailsPage.getMyOrderModalCheckbox().then((checkbox) => {
-            cy.wrap(checkbox)
+          onOrderDetailsPage.getCancelMyOrderButton().click()
+          onOrderDetailsPage.getMyOrderModalCheckbox().then((chekbox) => {
+            cy.wrap(chekbox)
               .should('not.be.visible')
               .check({ force: true })
               .should('be.checked')
           })
-          onOrderDetailsPage
-            .getChangeMyOrderModalButton()
-            .should('contain', 'Change my order')
-            .click({ force: true })
-          // cy.wait(500)
-
-          onSideCartPage.getAvailableProductsInCartPanel().should('be.visible')
-
-          // check cart if any items are under any notifications and remove them
-          onSideCartPage
-            .getCloseSideCartButton()
-            .click({ force: true, multiple: true })
-          onSideCartPage.removeAllItemsUnderNotificationGroupsFromCart()
-
-          // Increase the Order total by adding more products to cart
-          onSearchResultsPage.searchAndAddAvailableWowItemsToCartUntilReachMinSpendThreshold(
-            'pantry',
-            60,
-            'Aisle'
-          )
-
-          onSideCartPage.getViewCartButton().click()
-
-          cy.intercept('api/v3/ui/fulfilment/windows?*').as('fulfilmentWindow')
-
-          onSideCartPage.getTotalAmountElement().then((totalAmount) => {
-            cy.wrap(totalAmount.text()).as('expectedTotalAmount')
-          })
-
-          onSideCartPage.gotoCheckout()
-
-          cy.wait('@fulfilmentWindow')
-
-          const expectedTotalAmountAlias = 'expectedTotalAmount'
-
-          onCheckoutPage.onCheckoutPaymentPanel
-            .getPaymentTotalAmountElement()
-            .then((totalAmount) => {
-              cy.wrap(totalAmount.text()).as(expectedTotalAmountAlias)
-            })
-
-          onCheckoutPage.onCheckoutPaymentPanel.payWithExistingCreditCard(
-            '0321',
-            creditCardPayment.bb
-          )
+          onOrderDetailsPage.getCancelMyOrderModalButton().click()
           cy.wait(500)
-
-          // Verify order confirmation page
-
-          onOrderConfirmationPage.VerifyOrderConfirmationHeader()
-          onOrderConfirmationPage.VerifyTotalAmount(expectedTotalAmountAlias)
+          onOrderDetailsPage.getCancelledStatus().should('contain', 'Cancelled')
         }
       )
     })
